aca70daaf8fb4399faab792e68167f5f04a4dfc2
ganeshkumar383 <ganeshkumar383@gmail.com>
2020-07-20 17:34:30 +0530

    Base files

---
+++ TableQuery.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Flights(
+ID INT PRIMARY KEY, 
+AIRLINE_NAME VARCHAR(255),
+departure_time VARCHAR(255),
+arrival_time VARCHAR(255),
+number_of_stops VARCHAR(255),
+duration VARCHAR(255),
+price VARCHAR(255),
+source VARCHAR(255),
+dest VARCHAR(255),
+t_date VARCHAR(255)
+);
